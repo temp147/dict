d4e1ae2233d2c68a767904d6de5f03b5810bfb4b
temp147 <songy@ersoft.cn>
2025-03-31 22:17:39 +0800

    new file:   api/src/controllers/documents.ts 	new file:   api/src/database/seeds/58-documents.yaml 	new file:   api/src/services/documents.ts

---
+++ api/src/services/documents.ts
@@ -0,0 +1,15 @@
+
+import getDatabase from '../database/index.js';
+import type { AbstractServiceOptions } from '../types/index.js';
+import { ItemsService } from './items.js';
+
+
+export class DocumentsService extends ItemsService {
+	constructor(options: AbstractServiceOptions) {
+		super('nb_documents', options);
+
+		this.knex = options.knex || getDatabase();
+		this.accountability = options.accountability || null;
+		this.schema = options.schema;
+	}
+}
